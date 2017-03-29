2f82bbd053fae236f6e312b47e344c12a9a879e8
crmartn <cristian.martin.diaz.86@gmail.com>
2017-03-29 19:32:48 -0300

    first commit

---
+++ 01_helloWorld/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+  //fmt.Println("I fucking finally did it!")
+  //fmt.Println("After so many attempts!!!")
+  //fmt.Println("I'm telling you, Golang, I'm the fucking new programer of the block!!")
+}
